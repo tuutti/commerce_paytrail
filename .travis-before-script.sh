dce7fd3229a145805757d362a18b825f7153974c
Lari Rauno <tuutti@iki.fi>
2018-02-08 16:27:36 +0200

    Testing more stuff to fix travis

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -5,8 +5,6 @@ set -e $DRUPAL_TI_DEBUG
 # Ensure the right Drupal version is installed.
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
-cd "$DRUPAL_TI_DRUPAL_BASE"
-composer run-script drupal-phpunit-upgrade
 
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
