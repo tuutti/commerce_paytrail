9958f1648dcddd884736ea2f0d56bb08a44d7da4
Lari Rauno <tuutti@iki.fi>
2018-02-08 16:40:23 +0200

    php 7.2 fixes

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,5 +6,8 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
+cd "$DRUPAL_TI_DRUPAL_BASE"
+composer update phpunit/phpunit --with-dependencies --no-progress
+
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
