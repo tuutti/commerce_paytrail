4bb4ee041393001ba4410d648a207f800e5859b3
Lari Rauno <tuutti@iki.fi>
2018-02-08 18:07:17 +0200

    Run phantomjs later

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -11,6 +11,3 @@ if [ "$DRUPAL_TI_CORE_BRANCH" == "8.5.x" ]; then
 
   composer update phpunit/phpunit --with-dependencies --no-progress
 fi
-
-# Turn on PhantomJS for functional Javascript tests
-phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
