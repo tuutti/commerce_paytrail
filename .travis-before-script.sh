e1c4cc8d0792c84697adb7e54069615d6b2ff97b
Lari Rauno <tuutti@iki.fi>
2018-02-08 17:15:21 +0200

    Conditional core version

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -7,7 +7,10 @@ set -e $DRUPAL_TI_DEBUG
 drupal_ti_ensure_drupal
 
 cd "$DRUPAL_TI_DRUPAL_BASE/drupal"
-composer update phpunit/phpunit --with-dependencies --no-progress
+
+if [ "$TRAVIS_PHP_VERSION" == "7.2"]; then
+  composer update phpunit/phpunit --with-dependencies --no-progress
+fi
 
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
