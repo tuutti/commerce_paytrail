7d9de1f9bd6ee2de217a3aa072b9adb83be005e2
tuutti <tuutti@iki.fi>
2019-08-30 18:40:42 +0300

    Fixed broken if condition

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,7 +6,7 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-if [ !${DRUPAL_TI_CORE_BRANCH} != "8.8.x" ]; then
+if [ ${DRUPAL_TI_CORE_BRANCH} != "8.8.x" ]; then
   cd "$DRUPAL_TI_DRUPAL_BASE/drupal"
   composer update phpunit/phpunit --with-dependencies --no-progress
 fi
