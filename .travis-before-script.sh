76329801153c9a52dfb5f9ac6f511d506ba3e3b7
tuutti <tuutti@iki.fi>
2019-08-30 18:47:48 +0300

    Dont update phpunit

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,10 +6,5 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-if [ ${DRUPAL_TI_CORE_BRANCH} != "8.8.x" ]; then
-  cd "$DRUPAL_TI_DRUPAL_BASE/drupal"
-  composer update phpunit/phpunit --with-dependencies --no-progress
-fi
-
 # Turn on chromdriver for functional Javascript tests
 chromedriver > /dev/null 2>&1 &
