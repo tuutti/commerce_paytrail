1f6b93c7046519065cbe758d8028bbe9208860e4
tuutti <tuutti@iki.fi>
2019-09-01 03:06:37 +0300

    Debug

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -2,10 +2,10 @@
 
 set -e $DRUPAL_TI_DEBUG
 
-composer why phpunit/phpunit
 # Ensure the right Drupal version is installed.
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
+composer why phpunit/phpunit
 
 # Turn on chromdriver for functional Javascript tests
 chromedriver > /dev/null 2>&1 &
