4ebf9e1c98f209e8ce211a2378baacba97bebd7f
tuutti <tuutti@iki.fi>
2019-09-02 16:01:58 +0300

    TEsting stuff

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -5,7 +5,6 @@ set -e $DRUPAL_TI_DEBUG
 # Ensure the right Drupal version is installed.
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
-composer why phpunit/phpunit
 
 # Turn on chromdriver for functional Javascript tests
 chromedriver > /dev/null 2>&1 &
--- tests/src/Kernel/DataIncludeTest.php
+++ tests/src/Kernel/DataIncludeTest.php
@@ -5,7 +5,6 @@ namespace Drupal\Tests\commerce_paytrail\Kernel;
 use Drupal\commerce_order\Adjustment;
 use Drupal\commerce_order\Entity\Order;
 use Drupal\commerce_order\Entity\OrderItem;
-use Drupal\commerce_paytrail\Exception\InvalidBillingException;
 use Drupal\commerce_price\Price;
 use Drupal\commerce_product\Entity\Product;
 use Drupal\commerce_product\Entity\ProductVariation;
